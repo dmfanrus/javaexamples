529dcb03f531ba05f520c4f26d7cb9899a7f0631
Mikhail Gurko <Mikhail_Gurko@epam.com>
2018-04-27 17:01:02 +0300

    Multithreading(2-4)

--- src/main/java/others/multithreading/Exercise2.java
+++ src/main/java/others/multithreading/Exercise2.java
@@ -1,5 +1,32 @@
 package others.multithreading;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
 public class Exercise2 {
 
+    long tickersSold;
+
+    final AtomicInteger ticketsTaken;
+
+    public Exercise2() {
+        tickersSold = 0;
+        ticketsTaken = new AtomicInteger(0);
+    }
+
+    public void performJob(){
+        IntStream.iterate(1, p->p+1)
+                .parallel()
+                .limit(10)
+                .forEach(i -> ticketsTaken.getAndIncrement());
+        IntStream.iterate(1, q->q+1)
+                .limit(5)
+                .parallel()
+                .forEach(i -> ++tickersSold);
+        System.out.println(ticketsTaken + " " + tickersSold);
+    }
+
+    public static void main(String[] args) {
+        new Exercise2().performJob();
+    }
 }
---
+++ src/main/java/others/multithreading/Exercise4.java
@@ -0,0 +1,31 @@
+package others.multithreading;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        List<Integer> original = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        List<Integer> copy1 = new CopyOnWriteArrayList<>(original);
+        for (Integer integer : copy1) {
+            copy1.remove(integer);
+        }
+
+        List<Integer> copy2 = Collections.synchronizedList(original);
+        for (Integer integer : copy2) {
+            copy2.remove(integer);
+        }
+
+        List<Integer> copy3 = new ArrayList<>(original);
+        for (Integer integer : copy3) {
+            copy3.remove(integer);
+        }
+
+        Queue<Integer> copy4 = new ConcurrentLinkedQueue<>(original);
+        for (Integer integer : copy4) {
+            copy4.remove(integer);
+        }
+    }
+}
