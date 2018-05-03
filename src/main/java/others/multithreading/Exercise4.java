905191abd23ddccea3500608c718d97b769793fa
Mikhail Gurko <Mikhail_Gurko@epam.com>
2018-05-03 16:40:20 +0300

    Multithreading(4-7)

--- src/main/java/others/multithreading/Exercise4.java
+++ src/main/java/others/multithreading/Exercise4.java
@@ -1,31 +1,11 @@
 package others.multithreading;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Exercise4 {
+
     public static void main(String[] args) {
-        List<Integer> original = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
-
-        List<Integer> copy1 = new CopyOnWriteArrayList<>(original);
-        for (Integer integer : copy1) {
-            copy1.remove(integer);
-        }
-
-        List<Integer> copy2 = Collections.synchronizedList(original);
-        for (Integer integer : copy2) {
-            copy2.remove(integer);
-        }
-
-        List<Integer> copy3 = new ArrayList<>(original);
-        for (Integer integer : copy3) {
-            copy3.remove(integer);
-        }
-
-        Queue<Integer> copy4 = new ConcurrentLinkedQueue<>(original);
-        for (Integer integer : copy4) {
-            copy4.remove(integer);
-        }
+        ExecutorService executorService = Executors.newCachedThreadPool();
     }
 }
---
+++ src/main/java/others/multithreading/Exercise7.java
@@ -0,0 +1,25 @@
+package others.multithreading;
+
+import java.util.concurrent.*;
+
+public class Exercise7 {
+    public long multTwice(int i){
+        return i*2;
+    }
+
+    public void submitReports() throws ExecutionException, InterruptedException {
+        ExecutorService s = Executors.newCachedThreadPool();
+        Callable hello = () -> {
+            System.out.println("Hello");
+            return 1;
+        };
+        Object o = s.submit(hello).get();
+        s.shutdown();
+        multTwice((Integer) o);
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new Exercise7().submitReports();
+    }
+}
