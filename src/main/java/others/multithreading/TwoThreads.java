a41e33c9dfdd167bdf8e1996f6143f899fa84fee
Mikhail Gurko <Mikhail_Gurko@epam.com>
2018-05-24 17:38:38 +0300

    Master_3

--- src/main/java/others/multithreading/TwoThreads.java
+++ src/main/java/others/multithreading/TwoThreads.java
@@ -23,5 +23,6 @@ public class TwoThreads extends Thread{
         System.out.println("end_master_only_3");
         System.out.println("end_master_only_6");
         System.out.println("end_master_only_7");
+        System.out.println("end_master_only_8");
     }
 }
