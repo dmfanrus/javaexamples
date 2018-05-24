d1e3b75ff3a37ab9c8bf2dfea8475e28cacc8c7c
Mikhail Gurko <Mikhail_Gurko@epam.com>
2018-05-24 14:18:08 +0300

    rebase_commit

--- src/main/java/others/multithreading/TwoThreads.java
+++ src/main/java/others/multithreading/TwoThreads.java
@@ -3,6 +3,7 @@ package others.multithreading;
 public class TwoThreads extends Thread{
 
     String msg = "default";
+    String master = "master";
 
     public TwoThreads(String s) {
         msg = s;
@@ -15,6 +16,6 @@ public class TwoThreads extends Thread{
     public static void main(String[] args) {
         new TwoThreads("String1jhjhjgjg").run();
         new TwoThreads("String2hjjhj").run();
-        System.out.println("end_master");
+        System.out.println("end_master_only");
     }
 }
